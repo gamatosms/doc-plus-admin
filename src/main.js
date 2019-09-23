3a41bbb21b12412a6da206b14fd0e626a570d4db
gabriel <gsilva505@hotmail.com>
2019-09-23 18:17:11 -0300

    login

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vueButtonEffect from 'vue-button-effect'
+
+
+Vue.use(vueButtonEffect)
 
 Vue.config.productionTip = false
 
---
+++ src/views/login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+      <h1>Login</h1>
+      <div class="container login">
+          <label for="#">Código de acesso</label>
+          <input type="text">
+          <label for="#">Username/Email</label>
+          <input type="text">
+          <label for="#">Senha</label>
+          <input type="password"  name="senha">
+          <g-button-wave color="primary" id="button-login" size="button--size-m">Log in</g-button-wave>
+          <a href="#">Esqueceu a senha?</a>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .container{
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    label{
+        margin-bottom: 5px;
+    }
+    input{
+        margin-bottom: 30px;
+        width: 250px;
+        height: 30px;
+        border-radius: 100px;
+        outline: none;
+        padding-left: 10px;
+    }
+    #button-login{
+        margin-bottom: 30px;
+    }
+</style>
